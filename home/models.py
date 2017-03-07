e31bef2d9ea5e00ed6adc71f2156f7d89a2b31cd
Melanie Zhang <melanizhang@gmail.com>
2017-03-07 02:08:54 -0800

    fix minor database issues

--- home/models.py
+++ home/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class ParkingSpot(models.Model):
-    owner = models.CharField(max_length=25)
-    title = models.CharField(max_length=25)
-    zip = models.CharField(max_length=5)
-    street = models.CharField(max_length=50)
-    city = models.CharField(max_length=15)
-    state = models.CharField(max_length=2)
-    price = models.IntegerField()
+    owner = models.CharField(max_length=25, default="")
+    title = models.CharField(max_length=25, default="")
+    zipcode = models.CharField(max_length=5, default="")
+    street = models.CharField(max_length=50, default="")
+    city = models.CharField(max_length=15, default="")
+    state = models.CharField(max_length=2, default="")
+    price = models.IntegerField(default=0)
     picture = models.FileField(null=True)
 
     def __str__(self):
